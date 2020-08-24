7c6a69f6e26e30bfa8dd820af3b1acfda87050d3
David L. Inman <davidlevere@gmail.com>
2020-08-23 23:48:17 -0400

    copy over main and clean it out

---
+++ main.lua
@@ -0,0 +1,40 @@
+
+
+-- this only gets called once at the beginning
+function love.load()
+
+end
+
+-- this function is called continuously
+-- dt is the delta time (in seconds) of the last
+-- time that the function was called
+function love.update(dt)
+
+end
+
+-- this is the only function that the graphics functions
+-- will work in
+function love.draw()
+    
+end
+
+function love.mousepressed(x, y, button, istouch)
+    
+end
+
+function love.mousereleased(x, y, button, istouch)
+
+end
+
+function love.keypressed(key)
+end
+
+function love.keyreleased(key)
+end
+
+function love.focus(in_focus)
+
+end
+
+function love.quit()
+end
