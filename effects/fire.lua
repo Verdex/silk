bb2e032497ac179b5660dc00a59de92756c47fdf
David L. Inman <davidlevere@gmail.com>
2020-09-10 22:59:48 -0400

    fire effect

--- effects/fire.lua
+++ effects/fire.lua
@@ -1,4 +1,54 @@
 
+local util = require 'util'
 
-return { 
+local function create(x, y)
+
+    local obj = {x = x; y = y}
+
+    obj.interface = 'fire'
+
+    obj.visible = true
+    obj.lengths = {}
+    obj.length = 10 
+    obj.dt = 0
+    obj.id = util.gen_id()
+
+    for i = 1, obj.length do
+        obj.lengths[i] = { 0, 0, 0 }
+    end
+
+    function obj:update(dt) 
+        if self.dt + dt < 0.15 then
+            self.dt = self.dt + dt 
+        else
+            self.dt = 0
+            for i = 1, self.length do
+                for d = 1, 3 do
+                    self.lengths[i][d] = love.math.random(3, 10)
+                end
+            end
+        end
+    end
+
+    function obj:draw()
+        love.graphics.setColor(1.0, 1.0, 1.0)
+        for k, v in ipairs(self.lengths) do
+            love.graphics.line(self.x + k, self.y, self.x + k, self.y - v[1])
+        end
+
+        love.graphics.setColor(1.0, 1.0, 0)
+        for k, v in ipairs(self.lengths) do
+            love.graphics.line(self.x + k, self.y, self.x + k, self.y - v[2])
+        end
+
+        love.graphics.setColor(1.0, 0.5, 0)
+        for k, v in ipairs(self.lengths) do
+            love.graphics.line(self.x + k, self.y, self.x + k, self.y - v[3])
+        end
+    end
+
+    return obj
+end
+
+return { create = create
        }
--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 
 local repo = require 'repo'
+local util = require 'util'
+
+local fire = require 'effects/fire'
 
 data = {}
 
@@ -9,6 +12,15 @@ function love.load()
     data.draw = repo.create_standard()
     data.update = repo.create_standard() 
 
+    local i = 0
+    while i < 100 do
+        local x = love.math.random(100, 200)
+        local y = love.math.random(50, 150)
+        local f = fire.create(200 - x, 200 + y)
+        data.draw:add(f)
+        data.update:add(f)
+        i = i + 1
+    end
 end
 
 -- this function is called continuously
@@ -18,7 +30,7 @@ function love.update(dt)
 
     local remove = {}
     for _, v in data.update:iter() do
-        v.update(dt)     
+        v:update(dt)     
         if v.remove_update then
             remove[#remove+1] = v.id
         end
@@ -33,11 +45,13 @@ end
 -- this is the only function that the graphics functions
 -- will work in
 function love.draw()
+
+    love.graphics.clear()
    
     local remove = {}
     for _, v in data.draw:iter() do
         if v.visible then
-            v.draw()
+            v:draw()
         end
         if v.remove_draw then
             remove[#remove+1] = v.id 
