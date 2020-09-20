07b231af1efe2acfe820f6e672993b25d9c991a4
David L. Inman <davidlevere@gmail.com>
2020-09-20 16:17:43 -0400

    love examples

---
+++ example.lua
@@ -0,0 +1,18 @@
+
+local function x()
+    love.graphics.print("blarg", 200, 200)
+end
+
+--love.math.random(4, 5)
+--love.graphics.setColor(1,2,3,4)
+--love.graphics.line(x, y, x, y)
+--love.graphics.circle(mode, x, y, rad, segments)
+--love.graphics.arc(mode, x, y, rad, angle1, angle2, segments)
+--love.graphics.clear()
+--love.graphics.point(x,y)
+--love.graphics.points(x,y,..)
+
+-- mode = line | fill
+
+return { x = x }
+
